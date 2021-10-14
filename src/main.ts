ccf0675100d9139c9e69d116f636e25e6ae278c8
Crisoforo Lopez <43754109+chr1soscl@users.noreply.github.com>
2021-10-14 00:37:09 -0400

    sass override

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import './app.scss'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 
 Vue.config.productionTip = false
 
